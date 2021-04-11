9695062f879623989a823eead3339d74a8334f5c
Трпковска Јана <jana.trpkovska@students.finki.ukim.mk>
2021-04-11 17:44:22 +0200

    Json file read

--- challenge.php
+++ challenge.php
@@ -6,9 +6,28 @@
 <div>
     Welcome
     Order by rating : <?php echo $_POST["orderByRating"]; ?>
+    <br>
     minimum Rating: <?php echo $_POST["minimumRating"]; ?>
+    <br>
     order by date: <?php echo $_POST["orderByDate"]; ?>
+    <br>
     priotize by text: <?php echo $_POST["prioritizeByText"]; ?>
+</div>
+
+<div>
+    <?php
+
+    //require "Review.php";
+    //$obj = new Review();
+
+    $Json = file_get_contents('C:\Users\Admin\PhpstormProjects\untitled\reviews.json',true);
+
+    $str = str_replace("\xEF\xBB\xBF",'',$Json);
+
+    $myarray = json_decode($str);
+
+    var_dump($myarray);
+    ?>
 </div>
 
 </body>
