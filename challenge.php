28f72c24642020bfc57e52c0bf3166f9741e8b72
Трпковска Јана <jana.trpkovska@students.finki.ukim.mk>
2021-04-11 23:59:06 +0200

    array with Review objects

--- challenge.php
+++ challenge.php
@@ -1,33 +1,69 @@
 <html>
 <body>
 
-<h1>Welcome to my home page!</h1>
+<h1>Filters</h1>
 
 <div>
-    Welcome
-    Order by rating : <?php echo $_POST["orderByRating"]; ?>
-    <br>
-    minimum Rating: <?php echo $_POST["minimumRating"]; ?>
-    <br>
-    order by date: <?php echo $_POST["orderByDate"]; ?>
-    <br>
-    priotize by text: <?php echo $_POST["prioritizeByText"]; ?>
+    <?php $byRating = $_POST["orderByRating"]; echo $byRating; ?>
+    <?php $minimumR = $_POST["minimumRating"]; echo $minimumR; ?>
+    <?php $date = $_POST["orderByDate"]; echo $date; ?>
+    <?php $text = $_POST["prioritizeByText"]; echo $text ?>
 </div>
 
 <div>
     <?php
 
-    //require "Review.php";
-    //$obj = new Review();
+    require "Review.php";
+
 
     $Json = file_get_contents('C:\Users\Admin\PhpstormProjects\untitled\reviews.json',true);
 
     $str = str_replace("\xEF\xBB\xBF",'',$Json);
 
-    $myarray = json_decode($str);
+    $array = json_decode($str);
 
-    var_dump($myarray);
+    $reviews = array();
+
+    for($i=0;$i<count($array);$i++)
+    {
+        $obj=$array[$i];
+
+        $r = new Review();
+
+        $r->id = $obj->id;
+        $r->href = $obj->href;
+        $r->isVerified = $obj->isVerified;
+        $r->logoHref = $obj->logoHref;
+        $r->numComments = $obj->numComments;
+        $r->numLikes = $obj->numLikes;
+        $r->numShares = $obj->numShares;
+        $r->rating = $obj->rating;
+        $r->reviewCreatedOn= $obj->reviewCreatedOn;
+        $r->reviewCreatedOnTime = $obj->reviewCreatedOnTime;
+        $r->reviewCreatedOnDate = $obj->reviewCreatedOnDate;
+        $r->photos= $obj->photos;
+        $r->reviewerEmail= $obj->reviewerEmail;
+        $r->reviewerId = $obj->reviewerId;
+        $r->reviewerName = $obj->reviewerName;
+        $r->reviewId = $obj->reviewId;
+        $r->reviewFullText = $obj->reviewFullText;
+        $r->reviewText = $obj->reviewText;
+        $r->reviewerUrl = $obj->reviewerUrl;
+        $r->source = $obj->source;
+        $r->sourceId = $obj->sourceId;
+        $r->sourceName= $obj->sourceName;
+        $r->sourceType = $obj->sourceType;
+        $r->tags = $obj->tags;
+
+        //echo $r->id . "<br>";
+        array_push($reviews,$r);
+    }
+
+    //echo $r->id;
+    //var_dump($array);
     ?>
+
+
 </div>
 
 </body>
